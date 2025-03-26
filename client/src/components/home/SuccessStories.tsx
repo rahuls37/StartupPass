e244b2e3306165b4c41a9e664d10c030eed73b95
rahuls37 <rahulsamantcoc119@gmail.com>
2025-03-26 04:22:53 +0000

    update photo

--- client/src/components/home/SuccessStories.tsx
+++ client/src/components/home/SuccessStories.tsx
@@ -23,7 +23,7 @@ const SuccessStories = () => {
       acquirerName: "Marco Rossi",
       acquirerRole: "Current CEO",
       testimonial: "When I acquired LearnLingo, it only offered Spanish and French courses. As a polyglot with teaching experience, I've expanded to 8 languages. It's incredibly rewarding to combine my passion with business.",
-      imageSrc: "https://images.unsplash.com/photo-1517486808906-6ca8b3f8e1c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1349&q=80"
+      imageSrc: "https://emeritus.org/in/wp-content/uploads/sites/3/2022/09/future-of-edtech.jpg.optimal.jpg"
     },
     {
       id: 3,
